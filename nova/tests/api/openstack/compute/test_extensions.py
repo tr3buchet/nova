a0c483eca915e71bde5e785a01c5d775c15840e4
Josh Kearney <josh@jk0.org>
2012-01-19 13:42:21 -0600

    Add RAX Server Bandwidth extension.

--- nova/tests/api/openstack/compute/test_extensions.py
+++ nova/tests/api/openstack/compute/test_extensions.py
@@ -179,6 +179,7 @@ class ExtensionControllerTest(ExtensionTestCase):
             "Rescue",
             "SchedulerHints",
             "SecurityGroups",
+            "ServerBandwidth",
             "ServerDiagnostics",
             "ServerStartStop",
             "SimpleTenantUsage",
