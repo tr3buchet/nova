46f721b330ad5053a10d7ad5d769d6df051d831b
Mark Washenberger <mark.washenberger@rackspace.com>
2012-02-06 21:07:14 -0500

    use project-id as tenant-id in glance plugin
    Change-Id: Ie7a73e8f20414cd8c236607ef5695d41741ad0d3

--- nova/virt/xenapi/vm_utils.py
+++ nova/virt/xenapi/vm_utils.py
@@ -583,6 +583,7 @@ def upload_image(context, session, instance, vdi_uuids, image_id):
               'glance_port': glance_port,
               'sr_path': get_sr_path(session),
               'auth_token': getattr(context, 'auth_token', None),
+              'project_id': context.project_id,
               'properties': properties}
 
     kwargs = {'params': pickle.dumps(params)}
@@ -916,6 +917,7 @@ def _fetch_vhd_image(context, session, instance, image_id):
     params = {'image_id': image_id,
               'uuid_stack': _make_uuid_stack(),
               'sr_path': get_sr_path(session),
+              'project_id': context.project_id,
               'auth_token': getattr(context, 'auth_token', None)}
 
     def pick_glance(params):
