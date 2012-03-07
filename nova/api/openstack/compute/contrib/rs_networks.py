cac470bc183de8f4165835b017e44ef31ceb77f2
Jason Kölker <jason@koelker.net>
2012-03-07 15:13:46 -0600

    blah squash
    Change-Id: I7105598ecfe174cb75c1617c0fecd4e794ddb10c

---
+++ nova/api/openstack/compute/contrib/rs_networks.py
@@ -0,0 +1,96 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011 Grid Dynamics
+# Copyright 2011 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from webob import exc
+
+from nova.api.openstack import extensions
+from nova import exception
+from nova import flags
+from nova import log as logging
+import nova.network.api
+
+
+FLAGS = flags.FLAGS
+LOG = logging.getLogger(__name__)
+authorize = extensions.extension_authorizer('compute', 'networks')
+
+
+class NetworkController(object):
+
+    def __init__(self, network_api=None):
+        self.network_api = network_api or nova.network.api.API()
+
+    def index(self, req):
+        context = req.environ['nova.context']
+        authorize(context)
+        networks = self.network_api.get_all(context)
+        return  {'networks': networks}
+
+    def show(self, req, id):
+        context = req.environ['nova.context']
+        authorize(context)
+        LOG.debug(_("Showing network with id %s") % id)
+        try:
+            network = self.network_api.get(context, id)
+        except exception.NetworkNotFound:
+            raise exc.HTTPNotFound(_("Network not found"))
+        return {'network': network}
+
+    def delete(self, req, id):
+        context = req.environ['nova.context']
+        authorize(context)
+        LOG.info(_("Deleting network with id %s") % id)
+        try:
+            self.network_api.delete(context, id)
+        except exception.NetworkNotFound:
+            raise exc.HTTPNotFound(_("Network not found"))
+        return exc.HTTPAccepted()
+
+    def create(self, req, id, body):
+        if not body:
+            raise exc.HTTPUnprocessableEntity()
+
+        context = req.environ['nova.context']
+        authorize(context)
+
+        network = body['network']
+        label = network['label']
+        cidr = network['cidr']
+
+        try:
+            network = self.network_api.create(context, label, cidr)
+        except Exception:
+            LOG.exception(_("Create networks failed"), extra=network)
+            raise exc.HTTPBadRequest("create_network failed.")
+
+        return {'network': network}
+
+
+class Rs_networks(extensions.ExtensionDescriptor):
+    """Admin-only Network Management Extension"""
+
+    name = "RSNetworks"
+    alias = "rs-networks"
+    namespace = "http://docs.openstack.org/ext/services/api/v1.1"
+    updated = "2012-03-07T09:46:43-05:00"
+
+    def get_resources(self):
+        ext = extensions.ResourceExtension('rs-networks',
+                                           NetworkController())
+        return [ext]
