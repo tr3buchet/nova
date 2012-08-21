44541c49444254d03da03d7767fa4bd5634ee78a
Chris Behrens <cbehrens@codestud.com>
2012-08-21 17:44:20 +0000

    Cells: add NoopQuotaDriver
    Can be used to disable quota checking in child cells by specifying:
    
    quota_driver=nova.quota.NoopQuotaDriver

--- nova/quota.py
+++ nova/quota.py
@@ -84,6 +84,17 @@ CONF = config.CONF
 CONF.register_opts(quota_opts)
 
 
+class NoopQuotaDriver(object):
+    """Quota Driver to use to disable quota checking."""
+    def count(self, *args, **kwargs):
+        return 0
+
+    def __getattr__(self, key):
+        def _noop_func(*args, **kwargs):
+            return
+        return _noop_func
+
+
 class DbQuotaDriver(object):
     """
     Driver to perform necessary checks to enforce quotas and obtain
